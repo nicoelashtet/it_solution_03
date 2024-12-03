775e201f1eb7acbb52a7d5fb4d18a8a13a091f24
pannyumon <pannyum@gmail.com>
2024-12-03 14:06:49 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,5 +1,5 @@
 setInterval(() => {
         const now = new Date();
         document.getElementById('clock').innerText = 
-            now.toLocaleTimeString('ja-JP', { timeZone: 'Asia/Phnom_Penh', hour: '2-digit', minute: '2-digit', second: '2-digit' });
+            now.toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
     }, 1000);
